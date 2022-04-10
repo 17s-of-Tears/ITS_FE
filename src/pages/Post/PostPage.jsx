09973c1a911a48d7c9dab7777e675c0e64638053
Jungjaeyeong <jjy8037@gmail.com>
2022-04-10 13:49:37 +0900

    제봉팍 도움

--- src/pages/Post/PostPage.jsx
+++ src/pages/Post/PostPage.jsx
@@ -3,10 +3,7 @@ import { MainContainer } from './styles'
 
 const PostPage = () => 
     <MainContainer>
-        <div>
-            <img className='post__back'src="images/logo.png" />
-            <p>같이 팀 프로젝트하실 분 구해요!</p>
-        </div>
+        
     </MainContainer>
 
     
