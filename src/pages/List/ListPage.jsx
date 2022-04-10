0251e4a8bafa4e4acef2eebb7157a3c6c66e0d8e
Jungjaeyeong <jjy8037@gmail.com>
2022-04-10 21:40:44 +0900

    만들기만들기

--- src/pages/List/ListPage.jsx
+++ src/pages/List/ListPage.jsx
@@ -1,40 +1,67 @@
 import React from 'react'
 import { MainContainer } from './styles'
 
-const ListPage = () => 
-    <MainContainer>
-        <div className='list__filter'>
-            <div className='list__filter_txt'>
-                <p>검색 필터</p>
-                <p className='list__filter_stxt'>| Filter</p>
-            </div>
-            <div className='list__filter_box'> 
-                <div>
-                    <select>
-                        <option>지역</option>
-                    </select>
-                    <select>
-                        <option>종류</option>
-                    </select>
-                    <select>
-                        <option>언어</option>
-                    </select>
-                </div>
-                <div>
-                    <select>
-                        <option>최신순</option>
-                    </select>
-                </div>
-            </div>
-        </div>
-        <div className='list__join'>
-            <div className='list__join_txt'>
-                <p>총 100개의 팀</p>
-                <p className='list__join_stxt'>관심있는 팀에 댓글을 남겨 팀에 참여해 보세요!</p>
-            </div>
-        </div>
-    </MainContainer>
-
-
+const ListPage = () => (
+	<MainContainer>
+		<div className="list__filter">
+			<div className="list__filter_txt">
+				<p>검색 필터</p>
+				<p className="list__filter_stxt">| Filter</p>
+			</div>
+			<div className="list__filter_box">
+				<div>
+					<select>
+						<option>지역</option>
+					</select>
+					<select>
+						<option>종류</option>
+					</select>
+					<select>
+						<option>언어</option>
+					</select>
+				</div>
+				<div>
+					<select>
+						<option>최신순</option>
+					</select>
+				</div>
+			</div>
+		</div>
+		<div className="list__join">
+			<div className="list__join_txt">
+				<p>총 100개의 팀</p>
+				<p className="list__join_stxt">
+					관심있는 팀에 댓글을 남겨 팀에 참여해 보세요!
+				</p>
+			</div>
+			<div className="list__join_pro">
+				<div className="list__join_box">
+					<p>프로젝트</p>
+					<p>같이 팀 프로젝트하실 분 구해요!</p>
+					<p>2022-04-01</p>
+					<p>댓글10 조회수100</p>
+				</div>
+				<div className="list__join_box">
+					<p>프로젝트</p>
+					<p>같이 팀 프로젝트하실 분 구해요!</p>
+					<p>2022-04-01</p>
+					<p>댓글10 조회수100</p>
+				</div>
+				<div className="list__join_box">
+					<p>프로젝트</p>
+					<p>같이 팀 프로젝트하실 분 구해요!</p>
+					<p>2022-04-01</p>
+					<p>댓글10 조회수100</p>
+				</div>
+				<div className="list__join_box">
+					<p>프로젝트</p>
+					<p>같이 팀 프로젝트하실 분 구해요!</p>
+					<p>2022-04-01</p>
+					<p>댓글10 조회수100</p>
+				</div>
+			</div>
+		</div>
+	</MainContainer>
+)
 
 export default ListPage
--- src/pages/List/styles.js
+++ src/pages/List/styles.js
@@ -23,8 +23,10 @@ export const MainContainer = styled.div`
 		align-items: center;
 	}
 
-	.list__join_txt {
+	.list__join {
 		padding: 50px 278px 50px 278px;
+	}
+	.list__join_txt {
 		display: flex;
 		align-items: center;
 		font-size: 20px;
@@ -34,5 +36,21 @@ export const MainContainer = styled.div`
 	.list__join_stxt {
 		font-size: 14px;
 		color: ${colors.gray_aa};
+	}
+	.list__join_pro {
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+		text-align: center;
+	}
+	.list__join_box {
+		margin-top: 80px;
+		padding: 30px;
+		width: 320px;
+		height: 320px;
+		background-color: white;
+		border: 1px solid ${colors.gray_ea};
+		border-radius: 5px;
+		box-shadow: 0 0 15px rgba(0, 0, 0, 0.2);
 	}
 `
